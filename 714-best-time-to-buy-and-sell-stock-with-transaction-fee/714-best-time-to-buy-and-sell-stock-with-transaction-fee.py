3223f8276efccf3d42656447917bbbb5007a1bea
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-03 12:43:05 +0530

    Time: 716 ms (93.97%), Space: 21.5 MB (34.48%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,24 +1,11 @@
 class Solution(object):
-    #https://leetcode.com/discuss/study-guide/1490172/Dynamic-programming-is-simple
-    #To further optimize the previous solution, we notice that if we want to calculate the values for pos, we need to calculate only values for pos + 1 and nothing else. Like we don't need values at pos + 2 etc. So we can throw away everything else.
+    #from solutions
+    #when bought = True, we either sell or do nothing
+    #when bought = False, we either buy or do nothing
+    #the same idea as previous solution, but using a complex but shorter implementation
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        dp = [0, 0]
-        for pos in reversed(range(len(prices))):
-            dp_old = dp
-            dp = [0, 0]
-
-            for bought in [True, False]:
-                max_profit = 0
-
-                if not bought:
-                    # Buy stock
-                    max_profit = max(max_profit, dp_old[True] - prices[pos] - fee)
-                else:
-                    # Sell stock
-                    max_profit = max(max_profit, dp_old[False] + prices[pos])
-
-                # Do nothing
-                max_profit = max(max_profit, dp_old[bought])
-
-                dp[bought] = max_profit
-        return dp[False]
+        cash, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        return cash
