8f6607a93dc5f956a654f741f86dfcd15d2f8040
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-03 12:33:34 +0530

    Time: 1396 ms (27.55%), Space: 23.2 MB (26.23%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,27 +1,23 @@
-from functools import lru_cache
-
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         #https://leetcode.com/discuss/study-guide/1490172/Dynamic-programming-is-simple
-        @lru_cache(None)
-        def dp(pos,Bought):
+        #iterative solution, where we are using a dp table(matrix) , 
+        #instead of a dp function 
+        
+        dp = [[0,0] for _ in range(0,len(prices)+1)]
+        for pos in reversed(range(0,len(prices))):
+            for Bought in [True, False]:
+                maxprofit = 0
             
-            maxprofit = 0
-            
-            if pos == len(prices):
-                return 0
-            
-            if Bought:
-                #selling
-                maxprofit = max(maxprofit, dp(pos + 1, False) + prices[pos] - fee)
+                if Bought:
+                    #selling
+                    maxprofit = max(maxprofit, dp[pos + 1][False] + prices[pos] - fee)  
+                else:
+                    #buying
+                    maxprofit = max(maxprofit, dp[pos + 1][True] - prices[pos])
                 
-            else:
-                #buying
-                maxprofit = max(maxprofit, dp(pos + 1, True) - prices[pos])
-                
-            #doing nothing
-            maxprofit = max(maxprofit, dp(pos + 1, Bought))
-            
-            return maxprofit
-            
-        return dp(0,False)
+                #doing nothing
+                maxprofit = max(maxprofit, dp[pos + 1][Bought])
+                    
+                dp[pos][Bought] = maxprofit
+        return dp[0][False]
