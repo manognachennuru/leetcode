834d46ce85eb90e0d1f22f56af28397970793b9c
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-03 12:40:30 +0530

    Time: 1028 ms (54.79%), Space: 21.1 MB (87.38%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,23 +1,24 @@
-class Solution:
+class Solution(object):
+    #https://leetcode.com/discuss/study-guide/1490172/Dynamic-programming-is-simple
+    #To further optimize the previous solution, we notice that if we want to calculate the values for pos, we need to calculate only values for pos + 1 and nothing else. Like we don't need values at pos + 2 etc. So we can throw away everything else.
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        #https://leetcode.com/discuss/study-guide/1490172/Dynamic-programming-is-simple
-        #iterative solution, where we are using a dp table(matrix) , 
-        #instead of a dp function 
-        
-        dp = [[0,0] for _ in range(0,len(prices)+1)]
-        for pos in reversed(range(0,len(prices))):
-            for Bought in [True, False]:
-                maxprofit = 0
-            
-                if Bought:
-                    #selling
-                    maxprofit = max(maxprofit, dp[pos + 1][False] + prices[pos] - fee)  
+        dp = [0, 0]
+        for pos in reversed(range(len(prices))):
+            dp_old = dp
+            dp = [0, 0]
+
+            for bought in [True, False]:
+                max_profit = 0
+
+                if not bought:
+                    # Buy stock
+                    max_profit = max(max_profit, dp_old[True] - prices[pos] - fee)
                 else:
-                    #buying
-                    maxprofit = max(maxprofit, dp[pos + 1][True] - prices[pos])
-                
-                #doing nothing
-                maxprofit = max(maxprofit, dp[pos + 1][Bought])
-                    
-                dp[pos][Bought] = maxprofit
-        return dp[0][False]
+                    # Sell stock
+                    max_profit = max(max_profit, dp_old[False] + prices[pos])
+
+                # Do nothing
+                max_profit = max(max_profit, dp_old[bought])
+
+                dp[bought] = max_profit
+        return dp[False]
