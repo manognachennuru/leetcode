afa9b891c21c3b1dee0dd1031bf6adef1fbece35
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-04 11:15:19 +0530

    Time: 32 ms (89.26%), Space: 13.8 MB (95.24%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,15 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        #print(n) n is integer
-        #n is int, we need to count '1' bits in binary representation
-        #count while you convert into binary representation
-        
+        #from discussions
+        '''If we have number n, then n&(n-1) will remove the rightmost in binary representation of n. For example if n = 10110100, then n & (n-1) = 10110100 & 10110011 = 10110000, where & means bitwize operation and. Very convinient, is it not? What we need to do now, just repeat this operation until we have n = 0 and count number of steps.'''
         count = 0
-        p = 32
-        
-        while n != 0:
-            if pow(2,p) <= n:
-                count += 1
-                n -= pow(2,p)
-            p -= 1
+        while n > 0:
+            n = n & (n-1)
+            count += 1
         return count
