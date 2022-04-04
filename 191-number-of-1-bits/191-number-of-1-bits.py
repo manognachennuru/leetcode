fcce7952167e178b093a02f380de61e32549d7eb
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-04 11:21:08 +0530

    Time: 32 ms (89.26%), Space: 13.8 MB (95.24%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,9 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         #from discussions
-        '''If we have number n, then n&(n-1) will remove the rightmost in binary representation of n. For example if n = 10110100, then n & (n-1) = 10110100 & 10110011 = 10110000, where & means bitwize operation and. Very convinient, is it not? What we need to do now, just repeat this operation until we have n = 0 and count number of steps.'''
+        '''to get last digit, you can do (n & 1) 
+        and increment count if that last digit is 1'''
         count = 0
         while n > 0:
-            n = n & (n-1)
-            count += 1
+            lastbit = n & 1
+            count += lastbit #adds either zero or one
+            n = n>>1
         return count
