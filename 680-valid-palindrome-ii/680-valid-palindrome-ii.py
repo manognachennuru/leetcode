f1207c26add3a83b9ba0e1c0f66fc50826b4ceba
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-05 09:56:45 +0530

    Time: 250 ms (27.66%), Space: 14.5 MB (52.63%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -10,26 +10,12 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1
-        foundDiff = False
         while left < right:
+            #found a mismatched pair - try both deletions
             if s[left] != s[right]:
-                if foundDiff:
-                    return False
-                foundDiff = True
-                if s[left] == s[right - 1] and s[left + 1] == s[right]:
-                    return self.checkPalindrome(s,left+1,right) or self.checkPalindrome(s,left,right-1)
-                
-                if s[left] == s[right - 1]:
-                    right -= 1
-                elif s[left + 1] == s[right]:
-                    left += 1
-                
-                else:
-                    #2 chars shld be deleted
-                    return False
-            else:
-                left += 1
-                right -= 1
+                return self.checkPalindrome(s,left+1,right) or self.checkPalindrome(s,left,right-1)
+            left += 1
+            right -= 1
         return True
     
                 
