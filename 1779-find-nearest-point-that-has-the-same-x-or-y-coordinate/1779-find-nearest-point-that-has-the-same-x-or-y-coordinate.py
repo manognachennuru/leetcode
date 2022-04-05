b72c7a553ea336efc7f494261ab642b39fd5d131
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-05 23:01:50 +0530

    Time: 1486 ms (5.03%), Space: 19.4 MB (14.65%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,19 +1,23 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
-        #manhattan = [least manhattan distance, corresponding least index with least manhattan distance]
+        # we just need to store least manhattan distance
+        # corresponding least index with least manhattan distance
+        
+        md = 10000
+        index = len(points)
         manhattan = [10000, len(points)]
         
         for i,point in enumerate(points):
             if x != point[0] and y != point[1]:
                 continue
             curr = abs(x-point[0]) + abs(y-point[1])
-            if curr < manhattan[0]:
+            if curr < md:
                 #found a point with lesser manhattan distance
-                manhattan[0] = curr
-                manhattan[1] = i
+                md = curr
+                index = i
             
             elif curr == manhattan[0]:
                 #check if index is lesser
-                manhattan[1] = min(i,manhattan[1])
+                index = min(i,index)
             
-        return manhattan[1] if manhattan[1] != len(points) else -1
+        return index if index != len(points) else -1
