b4e17deef4d22cc4755e66fb96e8d4eb80cdd842
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-07 20:47:15 +0530

    Time: 101 ms (31.54%), Space: 14.5 MB (32.75%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -4,16 +4,17 @@ class Solution:
             return True
         x1,y1 = coordinates[0]
         x2,y2 = coordinates[1]
+        
         if x2 == x1:
             #lies in x axis
-            axis = x1
             return all(x[0] == x1 for x in coordinates)
+        
         slope = (y2-y1)/(x2-x1)
         
         for i in range(1,len(coordinates)):
-            x1,y1 = coordinates[i-1]
             x2,y2 = coordinates[i]
             if x1 == x2:
+                #divide by zero
                 return False
             if slope != (y2-y1)/(x2-x1):
                 return False
