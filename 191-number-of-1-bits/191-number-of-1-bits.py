1e73d432578203fe6e474ee061f64a6099af8fc7
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2022-04-19 09:21:43 +0530

    Time: 35 ms (77.72%), Space: 14 MB (8.93%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,11 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        #from discussions
-        '''to get last digit, you can do (n & 1) 
-        and increment count if that last digit is 1'''
+        # solution 1 from youtube
+        # %2 will give us the last digit each time
+        # bit shift is CPU efficient instead of dividing everytime
         count = 0
-        while n > 0:
-            lastbit = n & 1
-            count += lastbit #adds either zero or one
-            n = n>>1
+        while n != 0:
+            count += n % 2
+            n = n >> 1
         return count
