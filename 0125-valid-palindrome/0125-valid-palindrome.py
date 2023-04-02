4afdd9bd851071cdff7a11d365a530f6807f602b
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2023-04-02 09:26:35 -0400

    Time: 55 ms (34.29%), Space: 14.7 MB (67.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,31 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        lst = []
-        for char in s:
-            if char.isalnum():
-                lst.append(char)
-        if lst[::-1] == lst:
+    def isalnum(c):
+        #my implementation of built-in function
+        if ord('A') < ord(c) < ord('Z') or ord('a') < ord(c) < ord('z') or ord('0') < ord(c) < ord('9'):
             return True
         return False
         
+        
+    def isPalindrome(self, s: str) -> bool:
+        #don't use all built in functions and extra memory
+        #learn to use two pointers.
+        
+        l = 0 #left pointer
+        r = len(s)-1 #right pointer 
+        
+        while l < r:
+            
+            while l<r and not s[l].isalnum():
+                l += 1
+                
+            while r>l and not s[r].isalnum():
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+            
+        return True
         
         
