a0c3feabd574c0f0427600de320e0c9b42ef6bd0
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2023-07-01 14:09:48 -0700

    Time: 43 ms (75.43%), Space: 16.4 MB (27.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,19 +1,15 @@
 class Solution:
     def helper(self, n, dp):
-        if dp[n] != 0:
-            return dp[n]
-        else:
-            for i in range(3,n+1):
-                dp[i] = self.helper(i-1, dp) + self.helper(i-2, dp)
-            return dp[n]
-    
+        if dp[n] == 0:
+            dp[n] = self.helper(n-1, dp) + self.helper(n-2, dp)
+        return dp[n]
+        
     def climbStairs(self, n: int) -> int:
         if n < 3:
             return n
         dp = [0]*(n+1)
-        dp[0] = 1
         dp[1] = 1
         dp[2] = 2
-        self.helper(n, dp)
-        return dp[n]
+        return self.helper(n,dp)
         
+    
