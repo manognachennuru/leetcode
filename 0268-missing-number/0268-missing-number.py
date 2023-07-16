eba17e74f60037919a17366907285833c1bc5f31
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2023-07-16 16:27:01 -0700

    Time: 150 ms (46.06%), Space: 17.7 MB (52.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,18 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #only use O(1) space
-        res = 0
-        for num in nums:
-            res = res | (1 << num)
-            
-        #res has all 1 bits and one 0 bit
-        #retreive the num corresponding to the 0 bit
-        count = 0
-        while res > 0:
-            if res & 1:
-                res = res >> 1
-                count += 1
-            else:
-                return count
-        return count
+        #XOR in one loop
+        ans = len(nums)
+        for i, num in enumerate(nums):
+            ans ^= i
+            ans ^= num
+        return ans
             
