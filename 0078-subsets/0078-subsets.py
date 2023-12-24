33906f94f2f18614b76c7da7be5ed41153ed9eb9
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2023-12-25 00:22:01 +0530

    Time: 22 ms (17.06%), Space: 13.5 MB (29.59%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,27 +1,19 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        #backtracking - just form UNIQUE subsets
-        result = []
+class Solution(object):
+    def subsets(self, nums):
+        res = [[]]
         
-        subset = []
-        def dfs(i):
-            #base case
-            if i >= len(nums):
-                result.append(subset.copy())
-                return
+        def helper(n):
+            size = len(res)
+            for i in range(0, size):
+                #print("res[i] = ", res[i], "type(res[i]) = ", type(res[i]))
+                subset = res[i][:]
+                subset.append(n)
+                res.append(subset)
                 
-            #for all elements, we have 2 options. either to add it or ignore it.
-            #choose that element
-            subset.append(nums[i])
-            dfs(i+1)
+        for num in nums:
+            helper(num)
             
-            #dont choose that element
-            subset.pop()
-            dfs(i+1)
-            
-            
-        dfs(0)
-        return result
-                    
-                
-                
+        return res
+        
+        
+        
