3b91b52255bd76c4b7820ded8a29d69694a6f4d9
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-18 00:32:14 +0530

    Time: 67 ms (91.45%), Space: 18.1 MB (86.01%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,33 +1,30 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        #dfs - recursive
-        #input is similar to adjacency list, no need to change
-        #NO NEED OF STACK VARIABLE IN DFS RECURSIVE
-        def dfs(node):
-            path.append(node)
-
-            if node == (n-1):
-                #found path
-                paths.append(path.copy())
-                return
-
-            neighbors = graph[node]
-            for neighbor in neighbors:
-                dfs(neighbor)
-                path.pop()
-
-                #print(stack, paths, path)
-            return 
+        #using bfs, 
+        #iterative, use a queue, add current path to the queue
+        #source is 0, destination is n-1
+        #seen = set()
+        n = len(graph)
+        q = deque()
+        q.append([0])
         
         paths = []
         path = []
-        n = len(graph)
-        dfs(0)
-        return paths
         
-        
+        while q:
+            path = q.pop()
+            lastv = path[-1] 
+            if lastv == (n-1):
+                paths.append(path.copy())
+                continue
             
+            for nei in graph[lastv]:
+                q.append(path + [nei])
                 
+        return paths
                 
+        
+        
+        
         
         
