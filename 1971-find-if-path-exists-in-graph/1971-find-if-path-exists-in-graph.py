733f8f6d37437000b989832341b6b494680e4fdf
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-18 00:14:31 +0530

    Time: 1798 ms (28.68%), Space: 120.6 MB (40.92%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,33 +1,33 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        #let's try doing dfs
-        #create an adjacency list first
-        adj_lis = defaultdict(list)
-        for edge in edges:
-            a,b = edge
-            adj_lis[a].append(b)
-            adj_lis[b].append(a)
+        #dfs - recursive
         
-        import collections
-        q = deque()
-        q.append(source)
+        #create adjacency list
+        adj_list = [[] for i in range(n)]
+        for edge in edges:
+            a, b = edge
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+            
+        
+        
         seen = set()
         
-        while q:
-            node = q.popleft()
-            
+        def dfs(node):
+            #print("entered dfs with node = ", node)
             if node == destination:
                 return True
             
-            if node in seen:
-                continue
-            seen.add(node)           
-
-            for neighbor in adj_lis[node]:
-                q.append(neighbor)
+            seen.add(node)
+            for nei in adj_list[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    if dfs(nei):
+                        return True
+                else:
+                    continue
                 
-        return False
-            
-            
-            
-                
+            return False
+        
+        return dfs(source)
+        
