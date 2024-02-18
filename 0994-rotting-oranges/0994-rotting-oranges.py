d67a26ac694889fe7722e5b825faa549a48edab3
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-18 21:06:44 +0530

    Time: 53 ms (49.22%), Space: 16.5 MB (81.90%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -14,44 +14,37 @@ class Solution:
         cols = len(grid[0])
         minutes = 0
         visited = set()
+        fresh = 0
         
-        while True:
-             
-            statusChange = False
-            #print("minutes = ", minutes)
-            for r in range(0, rows):
-                for c in range(0, cols):
-                    if grid[r][c] == 2: #rotten orange
-                        if (r, c) not in visited:
-                            q.append((r,c)) 
-                            visited.add((r,c))
-                        
+        #print("minutes = ", minutes)
+        for r in range(0, rows):
+            for c in range(0, cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2: #rotten orange
+                    if (r, c) not in visited:
+                        q.append((r,c)) 
+                        visited.add((r,c))
 
-            while q:
+        while q and fresh:
+            for i in range(len(q)):
                 r, c = q.popleft() #always pop left for queue
-                
+
                 neighbors = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
                 #print("rotting orange is at ", r, c)
                 for nr, nc in neighbors: 
                     #check if indices are valid
-                    #print("nieghbors are ", nr, nc)
+                    #print("neighbors are ", nr, nc)
                     if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
                         continue
                     if grid[nr][nc] == 1:
                         grid[nr][nc] = 2
-                        statusChange = True
-            
-            if statusChange == False:
-                break
+                        q.append((nr,nc))
+                        fresh -= 1
             
             minutes += 1 
-                
-        #now check if you have a fresh orange or not
-        for r in range(0, rows):
-            for c in range(0, cols):
-                if grid[r][c] == 1: #fresh orange
-                    return -1
-        return minutes
+        
+        return minutes if not fresh else -1
         
             
                     
