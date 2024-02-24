3c97a405b96eb4f96d2594076f89b7e2ffdf31f3
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-25 02:46:48 +0530

    Time: 1092 ms (67.88%), Space: 107.2 MB (49.73%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,66 +1,72 @@
-class Solution:
-    # Testing editorial solution.
-    
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        if not points or len(points) == 0:
-            return 0
-        size = len(points)
-        pq = []
-        uf = UnionFind(size)
-
-        for i in range(size):
-            x1, y1 = points[i]
-            for j in range(i + 1, size):
-                x2, y2 = points[j]
-                # Calculate the distance between two coordinates.
-                cost = abs(x1 - x2) + abs(y1 - y2)
-                edge = Edge(i, j, cost)
-                pq.append(edge)
-        
-        # Convert pq into a heap.
-        heapq.heapify(pq)
-
-        result = 0
-        count = size - 1
-        while pq and count > 0:
-            edge = heapq.heappop(pq)
-            if not uf.connected(edge.point1, edge.point2):
-                uf.union(edge.point1, edge.point2)
-                result += edge.cost
-                count -= 1
-        return result
-
-class Edge:
-    def __init__(self, point1, point2, cost):
-        self.point1 = point1
-        self.point2 = point2
-        self.cost = cost
-
-    def __lt__(self, other):
-        return self.cost < other.cost
-
-class UnionFind:
+# UnionFind class
+class unionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
-        self.rank = [1] * size
 
     def find(self, x):
         if x == self.root[x]:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
-
+		
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+            self.root[rootY] = rootX
 
-    def connected(self, x, y):
+    def isConnected(self, x, y):
         return self.find(x) == self.find(y)
+    
+class Edge:
+    def __init__(self, point1, point2, weight):
+        self.point1 = point1
+        self.point2 = point2
+        self.weight = weight
+        
+    def __lt__(self, other):
+        return self.weight < other.weight
+        
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # now, let's try using an edge class
+        
+        n = len(points)
+        uf = unionFind(n)
+        result = 0
+        count = 0 # to count number of edges
+        pq = []
+        num = 0
+        
+        for i in range(0, n-1):
+            for j in range(i+1, n):                    
+                xi, yi = points[i]
+                xj, yj = points[j]
+                
+                weight = abs(xi-xj) + abs(yi-yj)
+                
+                edge = Edge(i, j, weight)
+                pq.append(edge)
+                
+        # Convert pq into a heap.
+        heapq.heapify(pq)
+         
+        while pq and count != (n-1):
+                edge = heapq.heappop(pq)
+                
+                #if it isn't forming a cycle
+                if not uf.isConnected(edge.point1, edge.point2):
+                    #add edge to disjoint set
+                    uf.union(edge.point1, edge.point2)
+                    
+                    #add weight to result
+                    result += edge.weight
+                    count += 1
+        
+        return result
+        
+                
+                
+            
+        
+        
