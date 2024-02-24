835743638f0d6e24b825ee2e53f5273443867bf4
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-25 02:14:11 +0530

    Time: 1086 ms (68.14%), Space: 107.5 MB (49.49%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,73 +1,66 @@
-# UnionFind class
-class unionFind:
+class Solution:
+    # Testing editorial solution.
+    
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        if not points or len(points) == 0:
+            return 0
+        size = len(points)
+        pq = []
+        uf = UnionFind(size)
+
+        for i in range(size):
+            x1, y1 = points[i]
+            for j in range(i + 1, size):
+                x2, y2 = points[j]
+                # Calculate the distance between two coordinates.
+                cost = abs(x1 - x2) + abs(y1 - y2)
+                edge = Edge(i, j, cost)
+                pq.append(edge)
+        
+        # Convert pq into a heap.
+        heapq.heapify(pq)
+
+        result = 0
+        count = size - 1
+        while pq and count > 0:
+            edge = heapq.heappop(pq)
+            if not uf.connected(edge.point1, edge.point2):
+                uf.union(edge.point1, edge.point2)
+                result += edge.cost
+                count -= 1
+        return result
+
+class Edge:
+    def __init__(self, point1, point2, cost):
+        self.point1 = point1
+        self.point2 = point2
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
+        self.rank = [1] * size
 
     def find(self, x):
         if x == self.root[x]:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
-		
+
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
-            self.root[rootY] = rootX
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
 
-    def isConnected(self, x, y):
+    def connected(self, x, y):
         return self.find(x) == self.find(y)
-    
-class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # heh, graph problem to find min spanning tree
-        # can we use Kruskals algorithm ? 
-        # calculate all distances first
-        # make a dictionary with weight: point1, point2 : 
-        # sort them based on weights
-        # for each edge, if it doesn't make a cycle, add it's score
-        # add edge into visted set
-        # break when number of edges = n-1
-        
-        uf = unionFind(len(points))
-        result = 0
-        count = 0 # to count number of edges
-        Wedge = defaultdict(list)
-        num = 0
-        
-        for i in range(0, len(points)-1):
-            for j in range(i+1, len(points)):                    
-                xi, yi = points[i]
-                xj, yj = points[j]
-                
-                weight = abs(xi-xj) + abs(yi-yj)
-                Wedge[weight].append((points[i], points[j]))
-         
-        #print("Weights to edges = ", Wedge)
-        #print("vertexes to nums = ", vertextoNum)
-        #now we have a dict with weight: all edges with that weight
-        for weight in sorted(Wedge.keys()):
-            for edge in Wedge[weight]:
-                pointA, pointB = edge
-                
-                #if it isn't forming a cycle
-                if not uf.isConnected(points.index(pointA), points.index(pointB)):
-                    #add edge to disjoint set
-                    uf.union(points.index(pointA), points.index(pointB))
-                    
-                    #add weight to result
-                    #print("adding edge = (",pointA, pointB,") and the weight is", weight)
-                    result += weight
-                    count += 1
-                
-                    if count == (len(points)-1):
-                        #formed MST
-                        return result
-        
-        return result
-        
-                
-                
-            
-        
-        
