0c5823d0b84debf4b53748c140c104412634973c
manognachennuru <49646836+manognachennuru@users.noreply.github.com>
2024-02-25 19:03:16 -0500

    Time: 90 ms (63.78%), Space: 18.5 MB (59.10%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,61 +1,32 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #you might have a cyclic or acyclic graph. 
-        # with a cyclic graph, we can't finish all the courses
-        
-        adj = [[] for _ in range(0,numCourses)]        #bi --> ai
-        indegree = [0 for _ in range(0, numCourses)]
-        
-        #first create adjacency list
-        for a, b in prerequisites:
-            if a == b:
-                return False
-            adj[b].append(a)
-            indegree[a] += 1
-            
-        visited = set()
-        finished = set()
-        node = None
-        
-        
-        
-        def dfs(node):
-            #base case? 
-            # print("node = ", node)
-            
-            visited.add(node)
-            for nei in adj[node]:
-                if nei not in visited:
-                    visited.add(nei)
-                    if dfs(nei) == True:
-                        return True
-                        
-                if nei in visited and nei not in finished:
-                    #found a cycle
-                    return True
-                
-            finished.add(node)
-        
-        
-        zero_indegree = []
-        #from where should you start dfs ? 
-        # a point where in degree is 0
-        for i in range(0, len(indegree)):
-            if indegree[i] == 0:
-                zero_indegree.append(i)
-        
-        # print("indegree = ", indegree)
-        # print("zero_indegree nodes are", zero_indegree)
-        
-        if len(zero_indegree) == 0:  #found no node with 0 indegree
+    #testing editorial solution..
+    
+    def dfs(self, node, adj, visit, inStack):
+        # If the node is already in the stack, we have a cycle.
+        if inStack[node]:
+            return True
+        if visit[node]:
             return False
-        
-        for node in zero_indegree:
-            #now call dfs
-            if dfs(node): #found a cycle --> can't finish courses
+        # Mark the current node as visited and part of current recursion stack.
+        visit[node] = True
+        inStack[node] = True
+        for neighbor in adj[node]:
+            if self.dfs(neighbor, adj, visit, inStack):
+                return True
+        # Remove the node from the stack.
+        inStack[node] = False
+        return False
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        for prerequisite in prerequisites:
+            adj[prerequisite[1]].append(prerequisite[0])
+
+        visit = [False] * numCourses
+        inStack = [False] * numCourses
+        for i in range(numCourses):
+            if self.dfs(i, adj, visit, inStack):
                 return False
-        
-        return True if len(finished) == numCourses else False
-        
+        return True
             
             
